2e6f5860c2c2342207acdc0780be588cae9ba2ca
Ryan L. Boyd <ryanb@ryanb.cc>
2018-03-12 19:43:08 -0500

    Initial commit

---
+++ PlugNChug/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace PlugNChug
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.VarBox = new System.Windows.Forms.TextBox();
+            this.CodeBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.BGWorker = new System.ComponentModel.BackgroundWorker();
+            this.AboutButton = new System.Windows.Forms.Button();
+            this.BenderBox = new System.Windows.Forms.PictureBox();
+            this.FryBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.BenderBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FryBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // VarBox
+            // 
+            this.VarBox.AcceptsReturn = true;
+            this.VarBox.AcceptsTab = true;
+            this.VarBox.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.VarBox.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VarBox.Location = new System.Drawing.Point(18, 49);
+            this.VarBox.MaxLength = 2147483647;
+            this.VarBox.Multiline = true;
+            this.VarBox.Name = "VarBox";
+            this.VarBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.VarBox.Size = new System.Drawing.Size(699, 166);
+            this.VarBox.TabIndex = 0;
+            this.VarBox.Text = "{{DV}} Happiness Sadness Depression";
+            this.VarBox.WordWrap = false;
+            // 
+            // CodeBox
+            // 
+            this.CodeBox.AcceptsReturn = true;
+            this.CodeBox.AcceptsTab = true;
+            this.CodeBox.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.CodeBox.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CodeBox.Location = new System.Drawing.Point(18, 265);
+            this.CodeBox.MaxLength = 2147483647;
+            this.CodeBox.Multiline = true;
+            this.CodeBox.Name = "CodeBox";
+            this.CodeBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.CodeBox.Size = new System.Drawing.Size(699, 194);
+            this.CodeBox.TabIndex = 1;
+            this.CodeBox.Text = "linear_model = lm({{DV}} ~ {{IV}}, data=df); summary(linear_model);";
+            this.CodeBox.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(414, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Variable Names (Separated by Spaces or Tabs):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 238);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Code to Fill In:";
+            // 
+            // StartButton
+            // 
+            this.StartButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartButton.Location = new System.Drawing.Point(238, 482);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(119, 42);
+            this.StartButton.TabIndex = 4;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.StatusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StatusLabel.Location = new System.Drawing.Point(18, 541);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(704, 28);
+            this.StatusLabel.TabIndex = 5;
+            this.StatusLabel.Text = "Ready to build code...";
+            this.StatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // BGWorker
+            // 
+            this.BGWorker.WorkerReportsProgress = true;
+            this.BGWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BGWorker_DoWork);
+            this.BGWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BGWorker_ProgressChanged);
+            this.BGWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BGWorker_RunWorkerCompleted);
+            // 
+            // AboutButton
+            // 
+            this.AboutButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AboutButton.Location = new System.Drawing.Point(379, 482);
+            this.AboutButton.Name = "AboutButton";
+            this.AboutButton.Size = new System.Drawing.Size(119, 42);
+            this.AboutButton.TabIndex = 6;
+            this.AboutButton.Text = "About";
+            this.AboutButton.UseVisualStyleBackColor = true;
+            this.AboutButton.Click += new System.EventHandler(this.AboutButton_Click);
+            // 
+            // BenderBox
+            // 
+            this.BenderBox.BackColor = System.Drawing.Color.Transparent;
+            this.BenderBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BenderBox.Location = new System.Drawing.Point(123, 477);
+            this.BenderBox.Name = "BenderBox";
+            this.BenderBox.Size = new System.Drawing.Size(71, 65);
+            this.BenderBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.BenderBox.TabIndex = 7;
+            this.BenderBox.TabStop = false;
+            // 
+            // FryBox
+            // 
+            this.FryBox.BackColor = System.Drawing.Color.Transparent;
+            this.FryBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.FryBox.Location = new System.Drawing.Point(536, 477);
+            this.FryBox.Name = "FryBox";
+            this.FryBox.Size = new System.Drawing.Size(71, 65);
+            this.FryBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.FryBox.TabIndex = 8;
+            this.FryBox.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(734, 578);
+            this.Controls.Add(this.AboutButton);
+            this.Controls.Add(this.StatusLabel);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CodeBox);
+            this.Controls.Add(this.VarBox);
+            this.Controls.Add(this.BenderBox);
+            this.Controls.Add(this.FryBox);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(750, 617);
+            this.MinimumSize = new System.Drawing.Size(750, 617);
+            this.Name = "MainForm";
+            this.Opacity = 0.93D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Plug N Chug v1.00";
+            ((System.ComponentModel.ISupportInitialize)(this.BenderBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FryBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox VarBox;
+        private System.Windows.Forms.TextBox CodeBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Label StatusLabel;
+        private System.ComponentModel.BackgroundWorker BGWorker;
+        private System.Windows.Forms.Button AboutButton;
+        private System.Windows.Forms.PictureBox BenderBox;
+        private System.Windows.Forms.PictureBox FryBox;
+    }
+}
+
---
+++ PlugNChug/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace PlugNChug
+{
+    public partial class MainForm : Form
+    {
+
+        public static string SoftwareNameString = "Plug N Chug (v1.00)";
+        public SoundPlayer snd = new SoundPlayer(Properties.Resources.getlucky);
+        public string LineCountString = "";
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            this.Text = SoftwareNameString;
+
+            VarBox.Text = "{{IV}} Open Consc Extra Agree Neuro" + "\r\n" +
+                          "{{DV}} Happiness Sadness Depression";
+
+            
+
+
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+
+            //BenderBox.Image = null;
+            //FryBox.Image = null;
+            VarBox.Enabled = false;
+            CodeBox.Enabled = false;
+            StartButton.Enabled = false;
+
+            BgWorkerInformation BgInfo = new BgWorkerInformation();
+
+            BgInfo.VarText = VarBox.Text;
+            BgInfo.CodeText = CodeBox.Text;
+
+            BGWorker.RunWorkerAsync(BgInfo);
+            
+        }
+
+
+        private void BGWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            BgWorkerInformation BgData = (BgWorkerInformation)e.Argument;
+            string[] VarLines = BgData.VarText.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+            
+
+            int NumVars = 0;
+            List<string> VarPlaceholders = new List<string>();
+
+            //get all of the split out info into a list
+            List<Array> VarList = new List<Array>();
+            for (int i = 0; i < VarLines.Length; i++) {
+
+                string[] VarList_Split = (VarLines[i].Trim().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries));
+                if (VarList_Split.Length > 1)
+                {
+                    NumVars++;
+                    VarList.Add(VarList_Split.ToList().GetRange(1, VarList_Split.Length - 1).ToArray());
+                    VarPlaceholders.Add(VarList_Split[0]);
+                }
+
+            }
+
+
+            try { 
+               
+
+                    StatusLabel.Invoke((MethodInvoker)delegate
+                    {
+                        StatusLabel.Text = "Recursing variable combinations... This might take a while...";
+                    });
+
+                //Set up our code
+
+
+                    List<string> RecursedVars = Recursion(0, VarList).Distinct().ToList();
+
+
+                    using (StreamWriter outputFile = new StreamWriter(new FileStream("PlugNChug_Code.txt", FileMode.Create, FileAccess.Write), Encoding.UTF8))
+                    {
+
+                    outputFile.WriteLine("Input Variable Data:");
+                    outputFile.WriteLine(BgData.VarText + "\r\n\r\n");
+                    outputFile.WriteLine("Input Code Data:");
+                    outputFile.WriteLine(BgData.CodeText + "\r\n\r\n");
+                    outputFile.WriteLine("Code Output:\r\n");
+
+                    int LineCount = RecursedVars.Count;
+                    LineCountString = LineCount.ToString();
+
+                        for (int i = 0; i < LineCount; i++)
+                        {
+
+                            StringBuilder OutputCode = new StringBuilder();
+                            OutputCode.Append(BgData.CodeText);
+
+                        StatusLabel.Invoke((MethodInvoker)delegate {
+                            StatusLabel.Text = "Writing line " + (i + 1).ToString() + " of " + LineCountString;
+                            StatusLabel.Invalidate();
+                            StatusLabel.Update();
+                            StatusLabel.Refresh();
+                            Application.DoEvents();
+                        });
+
+                        string[] Var_Replacements = RecursedVars[i].Split(' ');
+                            for (int j =0; j < NumVars; j++)
+                            {
+                                OutputCode.Replace(VarPlaceholders[j], Var_Replacements[j]);
+                            }
+
+                            outputFile.WriteLine(OutputCode.ToString());
+
+                    }
+
+
+
+                }
+                
+
+            }
+            catch
+            {
+                MessageBox.Show("An error occurred while building your code.", "Ruh-roh!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+
+        }
+
+
+
+        public class BgWorkerInformation
+        {
+            public string VarText { get; set; }
+            public string CodeText { get; set; }
+            
+        }
+
+        
+        //code borrowed / modified from here:
+        //https://stackoverflow.com/a/710716
+        static List<string> Recursion(int a, List<Array> x)
+        {
+            List<string> retval = new List<string>();
+
+            if (a == x.Count)
+            {
+                retval.Add("");
+                return retval;
+            }
+
+            foreach (Object y in x[a])
+            {
+                foreach (string x2 in Recursion(a + 1, x))
+                {
+                    retval.Add(y.ToString() + " " + x2.ToString());
+                    
+                }
+
+
+            }
+            return retval;
+        }
+
+
+
+
+
+
+        private void BGWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            StatusLabel.Text = "Finished!   :)";
+            DialogResult dialogResult = MessageBox.Show("Your code has been assembled!\r\n\r\nWould you like to open the output file?", "Finished!", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (dialogResult == DialogResult.Yes)
+                {
+                try
+                {
+                    System.Diagnostics.Process.Start("explorer.exe", "PlugNChug_Code.txt");
+                }
+                catch
+                {
+                    MessageBox.Show("There was a problem when trying to open your output file.", "Ruh-roh!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+                
+
+            CodeBox.Enabled = true;
+            VarBox.Enabled = true;
+            StartButton.Enabled = true;
+            
+        }
+
+        private void AboutButton_Click(object sender, EventArgs e)
+        {
+
+            try { 
+            snd.Stop();
+            snd.PlayLooping();
+            }
+            catch
+            {
+
+            }
+
+            BenderBox.Image = Properties.Resources.Bender;
+            FryBox.Image = Properties.Resources.Fry;
+
+            MessageBox.Show(SoftwareNameString + " by Ryan L. Boyd, Ph.D." + "\r\n\r\n" +
+                            "This software is explicitly for use in exploring your data and teaching about the dangers of multiple hypothesis testing. The onus is upon you, the user, to ensure that your statistical analyses / code are appropriate for your work and that all best standards / guidelines / practices are being followed with the utmost conscientiousness and rigor.",
+                            "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            
+
+        }
+
+        private void BGWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+        
+        }
+
+
+    }
+}
